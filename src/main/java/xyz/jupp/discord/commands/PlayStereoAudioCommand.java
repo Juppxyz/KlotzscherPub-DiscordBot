4262d9134eb8f61b35431bf0bcdacdc2249b4fe2
juppxyz <privat@jupp.xyz>
2023-06-04 21:33:23 +0200

    v0.12 b002
    (WIP)
    
    +ADD the PlayStereoAudio command
      -> Through this command, the Discord bot
         joins the VoiceChannel where the member is currently located
         and then plays the passed audio url.
    
    +ADD AudioPlayerSendHandler for handling audio interaction
    +ADD GuildMusicManager (basically a wrapper)
    +ADD TrackScheduler this creates a queue for play music like a "playlist" (WIP, experimental)

--- src/main/java/xyz/jupp/discord/commands/PlayStereoAudioCommand.java
+++ src/main/java/xyz/jupp/discord/commands/PlayStereoAudioCommand.java
@@ -1,5 +1,8 @@
 package xyz.jupp.discord.commands;
 
+import com.sedmelluq.discord.lavaplayer.player.AudioPlayer;
+import com.sedmelluq.discord.lavaplayer.track.playback.AudioFrame;
+import net.dv8tion.jda.api.audio.AudioSendHandler;
 import net.dv8tion.jda.api.entities.GuildVoiceState;
 import net.dv8tion.jda.api.entities.Member;
 import net.dv8tion.jda.api.entities.channel.concrete.TextChannel;
@@ -14,18 +17,21 @@ import xyz.jupp.discord.core.KlotzscherPubGuild;
 import xyz.jupp.discord.utils.EmbedMessageUtil;
 
 import java.awt.*;
+import java.nio.ByteBuffer;
 
 public class PlayStereoAudioCommand implements Command {
 
     private static final Logger log = LoggerFactory.getLogger(PlayStereoAudioCommand.class);
 
+    private static boolean isBotConnected = false;
+
     @Override
     public void action(SlashCommandInteractionEvent event) {
         String audioUrl = event.getOption("url", OptionMapping::getAsString);
         TextChannel textChannel = event.getChannel().asTextChannel();
+
         Member member = event.getMember();
         GuildVoiceState voiceState = member.getVoiceState();
-
         if (!voiceState.inAudioChannel()){
             event.replyEmbeds(
                     EmbedMessageUtil.buildSlashCommand(
@@ -38,17 +44,42 @@ public class PlayStereoAudioCommand implements Command {
 
         AudioChannelUnion voiceChannel = member.getVoiceState().getChannel();
         log.info("connect to voicechannel (" + voiceChannel.getId() + ")");
-        KlotzscherPubGuild.getGuild().getAudioManager().openAudioConnection(member.getVoiceState().getChannel());
+        //KlotzscherPubGuild.getGuild().getAudioManager().openAudioConnection(member.getVoiceState().getChannel());
 
 
         event.replyEmbeds(
                 EmbedMessageUtil.buildSlashCommand("⏱️ Deine aktive Zeit beträgt: ", Color.BLUE)
         ).setEphemeral(true).queue();
     }
-
     @Override
     public CommandOptions getCommandOptions() {
         return new CommandOptions("play", "Spielt eine Audio Datei in Stereo ab.");
+    }
+
+
+    public class AudioPlayerSendHandler implements AudioSendHandler {
+        private final AudioPlayer audioPlayer;
+        private AudioFrame lastFrame;
+
+        public AudioPlayerSendHandler(AudioPlayer audioPlayer) {
+            this.audioPlayer = audioPlayer;
+        }
+
+        @Override
+        public boolean canProvide() {
+            lastFrame = audioPlayer.provide();
+            return lastFrame != null;
+        }
+
+        @Override
+        public ByteBuffer provide20MsAudio() {
+            return ByteBuffer.wrap(lastFrame.getData());
+        }
+
+        @Override
+        public boolean isOpus() {
+            return true;
+        }
     }
 
 }
